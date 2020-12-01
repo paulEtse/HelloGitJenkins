591aa747516632538572ae224e7d65ecab875167
paulEtse <fiagnonpaule@gmail.com>
2020-12-01 14:37:31 +0100

    Initialisation

---
+++ SimpleClasse.java
@@ -0,0 +1,14 @@
+public class SimpleClasse {
+	public int somme(int a, int b){
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleClasse sc = new SimpleClasse();
+		int a = 3;
+		int b = 5;
+		System.out.println("La somme de "+a+" et "+b+" est : "+sc.somme(a,b));
+	}
+
+}
